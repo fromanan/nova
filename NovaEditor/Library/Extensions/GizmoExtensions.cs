4ecd4bffcbc793c8d7e1ec72d4dc57db05c57769
Tony Froman <33915703+fromanan@users.noreply.github.com>
2021-10-10 22:11:25 -0400

    Added GizmoExtensions to Editor DLL

---
+++ NovaEditor/Library/Extensions/GizmoExtensions.cs
@@ -0,0 +1,62 @@
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+
+namespace Nova.Library.Extensions
+{
+    public static class GizmoExtensions
+    {
+        public static void DrawWireCapsule(Vector3 pos, Quaternion rot, float radius, float height, Color color = default)
+        {
+            if (color != default) Handles.color = color;
+        
+            using (new Handles.DrawingScope(Matrix4x4.TRS(pos, rot, Handles.matrix.lossyScale)))
+            {
+                float pointOffset = (height - radius * 2f) / 2f;
+
+                // Draw sideways
+                Handles.DrawWireArc(Vector3.up * pointOffset, Vector3.left, Vector3.back, -180, radius);
+                Handles.DrawLine(new Vector3(0, pointOffset, -radius), new Vector3(0, -pointOffset, -radius));
+                Handles.DrawLine(new Vector3(0, pointOffset, radius), new Vector3(0, -pointOffset, radius));
+                Handles.DrawWireArc(Vector3.down * pointOffset, Vector3.left, Vector3.back, 180, radius);
+
+                // Draw front-ways
+                Handles.DrawWireArc(Vector3.up * pointOffset, Vector3.back, Vector3.left, 180, radius);
+                Handles.DrawLine(new Vector3(-radius, pointOffset, 0), new Vector3(-radius, -pointOffset, 0));
+                Handles.DrawLine(new Vector3(radius, pointOffset, 0), new Vector3(radius, -pointOffset, 0));
+                Handles.DrawWireArc(Vector3.down * pointOffset, Vector3.back, Vector3.left, -180, radius);
+
+                // Draw center
+                Handles.DrawWireDisc(Vector3.up * pointOffset, Vector3.up, radius);
+                Handles.DrawWireDisc(Vector3.down * pointOffset, Vector3.up, radius);
+            }
+        }
+    
+        public static void DrawWireCapsule(Transform transform, float radius, float height, Color color = default)
+        {
+            if (color != default) Handles.color = color;
+        
+            using (new Handles.DrawingScope(Matrix4x4.TRS(transform.position, transform.rotation, Handles.matrix.lossyScale)))
+            {
+                float pointOffset = (height - radius * 2f) / 2f;
+
+                // Draw sideways
+                Handles.DrawWireArc(Vector3.up * pointOffset, Vector3.left, Vector3.back, -180, radius);
+                Handles.DrawLine(new Vector3(0, pointOffset, -radius), new Vector3(0, -pointOffset, -radius));
+                Handles.DrawLine(new Vector3(0, pointOffset, radius), new Vector3(0, -pointOffset, radius));
+                Handles.DrawWireArc(Vector3.down * pointOffset, Vector3.left, Vector3.back, 180, radius);
+
+                // Draw front-ways
+                Handles.DrawWireArc(Vector3.up * pointOffset, Vector3.back, Vector3.left, 180, radius);
+                Handles.DrawLine(new Vector3(-radius, pointOffset, 0), new Vector3(-radius, -pointOffset, 0));
+                Handles.DrawLine(new Vector3(radius, pointOffset, 0), new Vector3(radius, -pointOffset, 0));
+                Handles.DrawWireArc(Vector3.down * pointOffset, Vector3.back, Vector3.left, -180, radius);
+
+                // Draw center
+                Handles.DrawWireDisc(Vector3.up * pointOffset, Vector3.up, radius);
+                Handles.DrawWireDisc(Vector3.down * pointOffset, Vector3.up, radius);
+            }
+        }
+    }
+}
+#endif
