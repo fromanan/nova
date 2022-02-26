614474e3e0b38c672bb0a88ec5fe1bb3895f8ac6
Tony Froman <33915703+fromanan@users.noreply.github.com>
2022-02-26 14:19:56 -0500

    Added NovaProcess wrapper class for Processes, Allows for easy logging

---
+++ NovaCore/Library/Files/NovaProcess.cs
@@ -0,0 +1,125 @@
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace NovaCore.Files
+{
+    public class NovaProcess
+    {
+        public Process Process { get; private set; }
+
+        public delegate void LogHandler(string s);
+        
+        public event LogHandler LogCallback = delegate {  };
+
+        public event LogHandler ErrorCallback = delegate {  };
+
+        public bool LogMessages;
+        
+        public bool LogErrors;
+        
+        public bool Started { get; private set; }
+        
+        public bool Completed { get; private set; }
+        
+        public bool SaveResult { get; private set; }
+        
+        public bool SaveErrors { get; private set; }
+        
+        public string Result { get; private set; }
+        
+        public string Errors { get; private set; }
+        
+        private readonly TaskCompletionSource<int> CompletionSource = new TaskCompletionSource<int>();
+        
+        public NovaProcess(string filepath, string args = "")
+        {
+            Process = FileSystem.CreateExternalProcess(filepath, args, false);
+        }
+        
+        public NovaProcess(ProcessStartInfo startInfo)
+        {
+            Process = FileSystem.CreateExternalProcess(startInfo);
+        }
+        
+        private void GetResult() => Result = Process.StandardOutput.ReadToEnd();
+        
+        private void GetErrors() => Result = Process.StandardError.ReadToEnd();
+        
+        public void Run()
+        {
+            if (Started) return;
+
+            Start();
+
+            Process.WaitForExit();
+
+            Stop();
+        }
+
+        public Task<int> RunAsync()
+        {
+            if (Started) return null;
+            
+            Process.EnableRaisingEvents = true;
+            
+            Process.Exited += (sender, args) =>
+            {
+                CompletionSource.SetResult(Process.ExitCode);
+                Stop();
+                Process.Dispose();
+            };
+            
+            Start();
+
+            return CompletionSource.Task;
+        }
+
+        private void Start()
+        {
+            if (LogMessages)
+            {
+                Process.OutputDataReceived += (sender, e) =>
+                {
+                    if (!string.IsNullOrEmpty(e.Data)) { LogCallback.Invoke(e.Data); }
+                };
+            }
+
+            if (LogErrors)
+            {
+                Process.ErrorDataReceived += (sender, e) =>
+                {
+                    if (!string.IsNullOrEmpty(e.Data)) { ErrorCallback.Invoke(e.Data); }
+                };
+            }
+
+            Process.Start();
+
+            if (LogMessages)
+            {
+                Process.BeginOutputReadLine();
+            }
+
+            if (LogErrors)
+            {
+                Process.BeginErrorReadLine();
+            }
+
+            Started = true;
+        }
+
+        private void Stop()
+        {
+            if (SaveResult)
+            {
+                GetResult();
+            }
+            
+            if (SaveErrors)
+            {
+                GetErrors();
+            }
+            
+            Completed = true;
+        }
+    }
+}
