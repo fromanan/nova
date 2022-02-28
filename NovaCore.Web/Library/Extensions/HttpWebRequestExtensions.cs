22b43bb49953e83b010756d690b0a9a7ae55bbc0
Tony Froman <33915703+fromanan@users.noreply.github.com>
2022-02-27 21:27:00 -0500

    Added WebHeader class, Added HttpWebRequestExtensions - AddHeaders

---
+++ NovaCore.Web/Library/Extensions/HttpWebRequestExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Net;
+
+namespace NovaCore.Web.Extensions
+{
+    public static class HttpWebRequestExtensions
+    {
+        public static void AddHeaders(this HttpWebRequest request, params WebHeader[] extraHeaders)
+        {
+            foreach (WebHeader header in extraHeaders)
+            {
+                request.Headers.Add(header.Key, header.Value);
+            }
+        }
+    }
+}
---
+++ NovaCore.Web/Library/Web/WebHeader.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Header = System.Collections.Generic.KeyValuePair<string, string>;
+
+namespace NovaCore.Web
+{
+    public class WebHeader : IFormattable
+    {
+        private readonly Header header;
+
+        public string Key => header.Key;
+
+        public string Value => header.Value;
+
+        public WebHeader(Header header)
+        {
+            this.header = header;
+        }
+        
+        public WebHeader(string key, string value)
+        {
+            header = new Header(key, value);
+        }
+
+        public static implicit operator Header(WebHeader d) => d.header;
+        
+        public static explicit operator WebHeader(Header b) => new WebHeader(b);
+
+        public override string ToString() => $"{header.Key} : {header.Value}";
+        
+        public string ToString(string format, IFormatProvider formatProvider)
+        {
+            return $"{header.Key} : {header.Value}";
+        }
+    }
+}
