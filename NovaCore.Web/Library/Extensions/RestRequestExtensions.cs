ec490538d03b67935f8366a8ba27bbd416ee76b3
Tony Froman <33915703+fromanan@users.noreply.github.com>
2022-02-27 21:27:21 -0500

    Add RestRequestExtensions - AddParameters method to RestRequest

---
+++ NovaCore.Web/Library/Extensions/RestRequestExtensions.cs
@@ -0,0 +1,15 @@
+﻿using RestSharp;
+
+namespace NovaCore.Web.Extensions
+{
+    public static class RestRequestExtensions
+    {
+        public static void AddParameters(this RestRequest request, params WebParameter[] parameters)
+        {
+            foreach (WebParameter parameter in parameters)
+            {
+                request.AddParameter(parameter.Key, parameter.Value);
+            }
+        }
+    }
+}
