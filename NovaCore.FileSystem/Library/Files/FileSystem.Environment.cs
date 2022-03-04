9c31038eb25c3cdb7b17d9f914e152d0c611e95f
Tony Froman <33915703+fromanan@users.noreply.github.com>
2022-03-04 14:57:47 -0500

    Added Environment and Registry Key support to FileSystem

---
+++ NovaCore.FileSystem/Library/Files/FileSystem.Environment.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+
+namespace NovaCore.Files
+{
+    public static partial class FileSystem
+    {
+        public static void SetEnvironmentVariable(string key, string value, bool overwrite = true)
+        {
+            // Check whether the environment variable exists.
+            string currentValue = Environment.GetEnvironmentVariable(key);
+            
+            // If necessary, create it.
+            if (string.IsNullOrEmpty(currentValue) || overwrite)
+            {
+                Environment.SetEnvironmentVariable(key, value);
+            }
+        }
+        
+        public static string GetEnvironmentVariable(string key)
+        {
+            return Environment.GetEnvironmentVariable(key);
+        }
+        
+        public static IDictionary GetEnvironmentVariables(string key)
+        {
+            return Environment.GetEnvironmentVariables();
+        }
+
+        public static void DeleteEnvironmentVariable(string key)
+        {
+            Environment.SetEnvironmentVariable(key, null);
+        }
+    }
+}
