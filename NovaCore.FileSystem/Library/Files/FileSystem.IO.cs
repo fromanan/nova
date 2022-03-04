207f63dfd42924a531b5727abf11b3425053632e
Tony Froman <33915703+fromanan@users.noreply.github.com>
2022-03-03 20:08:15 -0500

    Added Copy and Move to FileSystem.IO

--- NovaCore.FileSystem/Library/Files/FileSystem.IO.cs
+++ NovaCore.FileSystem/Library/Files/FileSystem.IO.cs
@@ -83,14 +83,6 @@ namespace NovaCore.Files
             return !(EmptyHierarchy(filepaths) || filepaths.Length == 1 && string.IsNullOrEmpty(filepaths[0]));
         }
 
-        // Copies a selected file to the downloads folder
-        public static string Download(string filepath)
-        {
-            string filename = Join(Paths.Downloads, Path.GetFileName(filepath));
-            File.Copy(filepath, filename);
-            return filename;
-        }
-
         public static string[] GetFiles(string directory, string searchPattern = "*")
         {
             return Directory.GetFiles(directory, searchPattern, SearchOption.TopDirectoryOnly);
@@ -155,16 +147,50 @@ namespace NovaCore.Files
                 .OrderByDescending(f => f.CreationTime)
                 .FirstOrDefault()?.FullName;
         }
-        
-        public static string Save(string body, string name, string extension, params string[] folderHierarchy)
+
+        // Copies a selected file to the downloads folder
+        public static string CopyToDownloads(string filepath)
         {
-            return SaveToFile(body, $"{TimestampFilename(name)}.{extension}", folderHierarchy);
+            return Copy(filepath, Paths.Downloads);
+        }
+        
+        public static string Copy(string origin, string destination)
+        {
+            string filename = Join(destination, GetFilename(origin));
+            File.Copy(origin, filename);
+            return filename;
+        }
+        
+        public static string Copy(string origin, params string[] folderHierarchy)
+        {
+            string destination = BuildFilepath(GetFilename(origin), folderHierarchy);
+            File.Copy(origin, destination);
+            return destination;
+        }
+
+        public static string Move(string origin, string destination)
+        {
+            string filename = Join(destination, GetFilename(origin));
+            File.Move(origin, filename);
+            return filename;
+        }
+        
+        public static string Move(string origin, params string[] folderHierarchy)
+        {
+            string destination = BuildFilepath(GetFilename(origin), folderHierarchy);
+            File.Move(origin, destination);
+            return destination;
         }
         
         public static string Download(string body, string name, string extension, params string[] folderHierarchy)
         {
             return SaveToFile(body, $"{TimestampFilename(name)}.{extension}",
                 folderHierarchy.Prepend(Paths.Downloads).ToArray());
+        }
+        
+        public static string Save(string body, string name, string extension, params string[] folderHierarchy)
+        {
+            return SaveToFile(body, $"{TimestampFilename(name)}.{extension}", folderHierarchy);
         }
     }
 }
